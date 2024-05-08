7685599c200c3255ffcaffa8f32010c49dd0a8b1
00007024 <164571431+00007024@users.noreply.github.com>
2024-05-08 12:52:15 -0600

    clase-8-mayo

---
+++ ejercicio01mayo8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char opcion,variable 'x';
+    do 
+    {
+        cout << "menu"<<"\n";
+
+        cout << "A carne asada"<<"\n";
+
+        cout << "B pollo"<<"\n";
+
+        cout << "C cerdo"<<"\n";
+
+        cout << "seleccione una opcion "<<"\n";
+
+        cin >> opcion;
+        cout << "opcion seleccionada"<< opcion << "\n";
+
+    }
+    while (opcion:x variable);
+    return 0;
+}
